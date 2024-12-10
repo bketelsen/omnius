327057c5b8875da642c9e6dba1dc0b88c90a42ae
Brian Ketelsen <bketelsen@gmail.com>
2024-12-10 11:07:59 -0500

    chore: import modules

--- web/routes_home.go
+++ web/routes_home.go
@@ -7,7 +7,7 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func setupHomeRoutes(r chi.Router, ns *embeddednats.Server) error {
+func setupHomeRoutes(r chi.Router, _ *embeddednats.Server) error {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		HomePage().Render(r.Context(), w)
 
